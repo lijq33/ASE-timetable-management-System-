b360759c07b68e1c680c54d6a6f3d411fc20c082
Supreme-leader-of-North-Korea <37832470+Supreme-leader-of-North-Korea@users.noreply.github.com>
2019-02-09 21:36:55 +0800

    Add MainApp, footer, header template. Add home, index, login, register page. Add routes general and store javascript, css and js. Add JWT auth
    Update User.php, auth.php, app.js and bootstrap.js

--- config/app.php
+++ config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /*
+        * Other Service Providers...
+        */
+        Nexmo\Laravel\NexmoServiceProvider::class,
     ],
 
     /*
